b803c5e322dad9009cb4c40a4acf43aa1fc0bf9a
Rustenis <r.tolpeznikas@gmail.com>
2024-11-13 22:28:29 +0100

    added .env usage

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 # This project was created to save me 70 euros from these ukrainian lads
 
+import os
+from dotenv import load_dotenv
 import asyncio
 import time
 from pyppeteer import launch
 
+# Load the environment variables
+load_dotenv()
+
 async def scraper():
     # Launch the browser
+    executable_path = os.getenv('BROWSER_PATH')
     browser = await launch({
         'headless': False,
-        'executablePath': '/usr/bin/brave-browser', # If nothing starts, this is the issue
+        'executablePath': executable_path, # If nothing starts, this is the issue
         })
     page = await browser.newPage()
 
-    await page.goto("https://www.google.com/")
+    await page.goto("https://platesmania.com/pl/gallery")
     time.sleep(5)
 
-    await page.screenshot({'path': 'example.png'})
+    await page.screenshot({'path': 'screenlog.png'})
 
     await browser.close()
 
